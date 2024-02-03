ee25843d59695dd5d910768e9a58cf730933db54
M4yG0M1ni <3164864582@qq.com>
2024-02-03 11:06:40 +0800

    modified:   MIT6.004/program/HuffmanTree.py

--- MIT6.004/program/HuffmanTree.py
+++ MIT6.004/program/HuffmanTree.py
@@ -1,5 +1,10 @@
 import copy
-Binary_Tree1 = ""   #前序遍历
+import turtle
+import math     #计算绘制角度
+
+LENGTH = 150     #设置二叉树最小边长度
+CIRCLE = 20      #设置圆框的半径
+
 class HuffmanTree(object):
     def __init__(self,string):
         self.string = string    #传入要处理的字符串
@@ -62,9 +67,7 @@ class HuffmanTree(object):
     
     def Encode(self,tree,encoding):     #递归方法对霍夫曼树进行编码
         if type(tree) != list:          #找到数据后对其进行编码
-            global Binary_Tree1
             self.dict[tree] = encoding
-            Binary_Tree1 += str(tree)    #对霍夫曼二叉树进行前序遍历
         else:                           #没找到继续向下递归
             self.Encode(tree[0],encoding+'0')
             self.Encode(tree[1],encoding+'1')
@@ -105,7 +108,132 @@ class Decode(object):
             print("Decode Error!")
             exit(0)
         return self.output
+    
 
+class Turtle(object):   #适用于所有二叉树
+    def __init__(self,encoding):
+        self.depth = -1     #深度从0开始计算
+        self.dict = encoding    #字典
+        self.tree = []
+        self.angle = []
+        self.lenth = []
+    
+    def Depth(self):
+        for part in self.dict:
+            if len(self.dict[part]) > self.depth:
+                self.depth = len(self.dict[part])
+        for i in range(self.depth):     #创建空的二叉树
+            self.tree.append(['nul' for j in range(int(math.pow(2,i+1)))])
+
+    def AngleLength(self):
+        for i in range(self.depth-1 , -1 , -1):
+            alpha = math.atan2(LENGTH*math.pow(2,i)/2 , LENGTH)     #alpha为弧度制
+            self.lenth.append(int(LENGTH / math.cos(alpha)))
+            alpha = int(alpha / math.pi * 180 )       #转化为整型角度制
+            self.angle.append(alpha)        #存储角度
+
+    def FindNode(self,part,encoding):
+        last = 0
+        for i in range(len(encoding)):
+            if len(encoding) == 1:
+                self.tree[i][int(encoding[0])+last*2] = part
+                encoding = ''
+            if len(encoding) > 1:
+                self.tree[i][int(encoding[0])+last*2] = 'val'
+                last = int(encoding[0])+last*2
+                encoding = encoding[1:]
+
+    def MakeTree(self):
+        for part in self.dict:
+            self.FindNode(part,self.dict[part])
+
+    def TurtleGo(self):
+        def AdvanceSetting():
+            turtle.setup(800,800,0,0)
+            turtle.speed(10)
+            turtle.hideturtle()
+            turtle.penup()         #抬起画笔
+            turtle.goto(0,350)
+            turtle.pendown()       #画笔落下
+            #画出第一个圆形
+            x = turtle.xcor()
+            y = turtle.ycor()
+            turtle.penup()         #抬起画笔
+            turtle.seth(270)
+            turtle.fd(CIRCLE)
+            turtle.seth(0)
+            turtle.pendown()       #画笔落下
+            turtle.circle(CIRCLE)
+            turtle.penup()         #抬起画笔
+            turtle.setx(x)
+            turtle.sety(y)
+            turtle.pendown()       #画笔落下
+
+        #depth = 0
+        def TurtleStep(length,angle,direc,val):     #画出去
+            if val == 'nul':
+                return 'nul'
+            if direc == "l":
+                direc = -1
+            elif direc == "r":
+                direc = 1
+            turtle.seth(270+angle*direc)
+            turtle.penup()         #抬起画笔
+            turtle.fd(CIRCLE)
+            turtle.pendown()       #画笔落下
+            turtle.fd(length)
+            turtle.penup()         #抬起画笔
+            turtle.fd(CIRCLE)
+            x = turtle.xcor()
+            y = turtle.ycor()
+            turtle.seth(270)
+            turtle.fd(CIRCLE)
+            turtle.seth(0)
+            turtle.pendown()       #画笔落下
+            turtle.circle(CIRCLE)
+            turtle.penup()         #抬起画笔
+            if val != 'val' :
+                val = '\"'+ val + '\"'
+                turtle.seth(270)
+                turtle.fd(CIRCLE)
+                turtle.seth(0)
+                for val0 in val:
+                    turtle.write(val0,font=("宋体",int(CIRCLE),'normal'))
+                    turtle.fd(CIRCLE/2)
+            turtle.setx(x)
+            turtle.sety(y)
+
+
+
+        def Recursion(depth,last):        #递归返回
+            if depth == self.depth:
+                return "depth"
+            x = turtle.xcor()
+            y = turtle.ycor()
+            TurtleStep(self.lenth[depth],self.angle[depth],'l',self.tree[depth][2*last+0])
+            Recursion(depth+1,2*last+0)
+            turtle.setx(x)
+            turtle.sety(y)
+            TurtleStep(self.lenth[depth],self.angle[depth],'r',self.tree[depth][2*last+1])
+            Recursion(depth+1,2*last+1)
+            turtle.setx(x)
+            turtle.sety(y)
+
+        AdvanceSetting()
+        Recursion(0,0)
+        turtle.done()
+        
+
+        
+
+    def DrawTree(self):
+        self.Depth()
+        self.AngleLength()
+        self.MakeTree()
+        print("重新编码得到的二叉树: ",self.tree)
+        print("画线长度: ",self.lenth)
+        print("线的角度: ",self.angle)
+        self.TurtleGo()
 
 def main():
     #tree = [['a',3],['b',5],['c',7],['d',8],['e',6],['f',4]]    #(字符,权重)
@@ -115,6 +243,8 @@ def main():
     string = input("请输入需要解码的二进制码: ")
     DecodeTree = Decode(encodings,string)
     print("解码结果:",DecodeTree.decoding())
+    Draw = Turtle(encodings)
+    Draw.DrawTree()
 
 if __name__ == '__main__':
     main()
